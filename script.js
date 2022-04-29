2e070f0b3d18b7dc51974b2c121eadac4d080479
luispaulobrito <luiss_paullo@hotmail.com>
2022-04-29 18:34:35 -0300

    commit api

---
+++ script.js
@@ -0,0 +1,18 @@
+const formulario = document.querySelector("form");
+const Inome = document.querySelector(".nome");
+const Iemail = document.querySelector(".email");
+const Isenha = document.querySelector(".senha");
+const Itel = document.querySelector(".tel");
+
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    const dados = {
+        nome: Inome.value,
+        email: Iemail.value,
+        senha: Isenha.value,
+        telefone: Itel.value
+    };
+
+    console.log(dados)
+});
